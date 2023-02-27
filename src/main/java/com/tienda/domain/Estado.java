e6757f01ba7982dd6a037d7b3e2f7f705f8a5656
YostinArias <yostinarias220@gmail.com>
2023-02-27 16:19:47 -0600

    Practica Commit final 1.1

--- src/main/java/com/tienda/domain/Estado.java
+++ src/main/java/com/tienda/domain/Estado.java
@@ -18,17 +18,19 @@ public class Estado implements Serializable {
     private String pais;
     private String capital;
     private String idioma;
+    private Long cantidad_Pob;
     private String codigo_postal;
 
     public Estado() {
     }
 
-    public Estado(Long id_estado, String nombre_est, String apellidos, String correo, String telefono) {
+    public Estado(Long id_estado, String nombre_est, String apellidos, String correo, Long cantidadPob,String telefono) {
         this.id_estado = id_estado;
         this.nombre_est = nombre_est;
          this.pais = pais;
           this.capital = capital;
            this.idioma = idioma;
+           this.cantidad_Pob = cantidad_Pob;
             this.codigo_postal = codigo_postal;
     }
 }
